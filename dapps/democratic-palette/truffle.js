f1d6d713e4a72bf655d1c596208aaa24ab0a7d03
Rob Myers <rob@robmyers.org>
2016-07-11 21:10:05 -0700

    Add Democratic Palette.

---
+++ dapps/democratic-palette/truffle.js
@@ -0,0 +1,24 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/jquery.min.js",
+      "javascripts/bootstrap.min.js",
+      "javascripts/Colour.js",
+      "javascripts/ColourPicker.js",
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/bootstrap.min.css",
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  },
+  deploy: [
+    "DemocraticPalette"
+  ],
+  rpc: {
+    host: "localhost",
+    port: 8545
+  }
+};
