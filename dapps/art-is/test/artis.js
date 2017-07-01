67c449df26c516992042c4bc555aa9591722bb47
Rob Myers <rob@robmyers.org>
2017-06-30 21:21:31 -0700

    Modernise js in art-is. Handle more error conditions. Add more tests (not all pass yet).

--- dapps/art-is/test/artis.js
+++ dapps/art-is/test/artis.js
@@ -1,90 +1,110 @@
-var ArtIs = artifacts.require("./ArtIs.sol");
+const ArtIs = artifacts.require("./ArtIs.sol");
 
-contract('ArtIs', function(accounts) {
-  it("Should initialise extent of definition #1 to 2", function () {
-    return ArtIs.deployed().then(function (instance) {
+contract('ArtIs', (accounts) => {
+  it("should initialise extent of definition #1 to 2", () => {
+    return ArtIs.deployed().then(instance => {
       return instance.definitions.call(1);
-    }).then (function (definition) {
+    }).then (definition => {
       assert.equal(definition[1].toNumber(), 2)
     });
   });
-  it("Should accept correctly specified changes of definition", function () {
-    var artis;
+  it("should accept correctly specified changes of definition", () => {
+    let artis;
     // 1000000000000000000 is the price for setting index 11
-    return ArtIs.deployed().then(function (instance) {
+    return ArtIs.deployed().then(instance => {
       artis = instance;
       return artis.setDefinition(11, 9, 8, 7, 6,
-                                 {from:accounts[1],
+                                 {from: accounts[1],
                                   value: 1000000000000000000});
-    }).then (function () {
+    }).then (() => {
       return artis.definitions.call(11);
-    }).then (function (definition) {
+    }).then (definition => {
       assert.equal(definition[0].toString(), accounts[1]);
       assert.equal(definition[1].toNumber(), 9);
       assert.equal(definition[2].toNumber(), 8);
       assert.equal(definition[3].toNumber(), 7);
       assert.equal(definition[4].toNumber(), 6);
-      var balance = web3.eth.getBalance(artis.address).toNumber();
+      const balance = web3.eth.getBalance(artis.address).toNumber();
       assert.equal(balance, 1000000000000000000);
     });
   });
-  it("Should not drain value of contract when non-owner asks", function () {
-    var artis;
-    var owner_old_balance;
+  it("should not drain value of contract when non-owner asks", () => {
+    let artis;
+    let owner_old_balance;
     // 500 is the price for setting index 3
-    return ArtIs.deployed().then(function (instance) {
+    return ArtIs.deployed().then(instance => {
       artis = instance;
       return artis.drain({from: accounts[1]});
     }).catch(function(error) {
         assert.notEqual(error.toString().indexOf("invalid JUMP"), -1);
     });
   });
-  it("Should drain value of account when owner asks", function () {
-    var artis;
-    var owner_old_balance;
+  it("should drain value of account when owner asks", () => {
+    let artis;
+    let owner_old_balance;
     // 500 is the price for setting index 3
-    return ArtIs.deployed().then(function (instance) {
+    return ArtIs.deployed().then(instance => {
       artis = instance;
       // Accounts[0] will be contract creator
       owner_old_balance = web3.eth.getBalance(accounts[0]).toNumber();
       return artis.drain();
-    }).then (function (result) {
-      var gasUsed = result.receipt.gasUsed * web3.eth.gasPrice.toNumber();
-      var new_balance = web3.eth.getBalance(accounts[0]).toNumber();
+    }).then (result => {
+      const gasUsed = result.receipt.gasUsed * web3.eth.gasPrice.toNumber();
+      const new_balance = web3.eth.getBalance(accounts[0]).toNumber();
       // Gas costs are hard. So just check that the balance is higher
       assert.isAbove(new_balance - gasUsed, owner_old_balance);
-      var balance = web3.eth.getBalance(artis.address).toNumber();
+      const balance = web3.eth.getBalance(artis.address).toNumber();
       assert.equal(balance, 0);
     });
   });
-  it("Should not accept definitions with bad values", function () {
-    var artis;
-    return ArtIs.deployed().then(function (instance) {
+  it("should not accept definitions with bad values", () => {
+    let artis;
+    return ArtIs.deployed().then(instance => {
       return instance.setDefinition(3, 900, 1, 1, 1,
-                                    {from:accounts[1],
+                                    {from: accounts[1],
                                      value: 500});
-    }).catch(function(error) {
+    }).catch(error => {
         assert.notEqual(error.toString().indexOf("invalid JUMP"), -1);
     });
   });
-  it("Should not accept definitions with a bad index", function () {
-    var artis;
-    return ArtIs.deployed().then(function (instance) {
+  it("should not accept definitions with a bad index", () => {
+    let artis;
+    return ArtIs.deployed().then(instance => {
       return instance.setDefinition(100, 1, 1, 1, 1,
-                                    {from:accounts[1],
+                                    {from: accounts[1],
                                      value: 500});
-    }).catch(function(error) {
+    }).catch(error => {
         assert.notEqual(error.toString().indexOf("invalid JUMP"), -1);
     });
   });
-  it("Should not accept definitions with a bad value", function () {
-    var artis;
-    return ArtIs.deployed().then(function (instance) {
+  it("should not accept definitions with a bad value", () => {
+    let artis;
+    return ArtIs.deployed().then(instance => {
       return instance.setDefinition(3, 1, 1, 1, 1,
-                                    {from:accounts[1],
+                                    {from: accounts[1],
                                      value: 5});
-    }).catch(function(error) {
+    }).catch(error => {
         assert.notEqual(error.toString().indexOf("invalid JUMP"), -1);
+    });
+  });
+  it("should not allow non-owner to transfer ownership", () => {
+    let artis;
+    return ArtIs.deployed().then(instance => {
+      return instance.transferOwnership(accounts[1], {from: accounts[1]});
+    }).then(() => {
+      return ArtIs.drain({from: accounts[1]});
+    }).catch(error => {
+        assert.equal(error.toString().indexOf("invalid JUMP"), -1);
+    });
+  });
+  it("should allow the owner to transfer ownership", () => {
+    let artis;
+    return ArtIs.deployed().then(instance => {
+      return instance.transferOwnership(accounts[1], {from: accounts[0]});
+    }).then(() => {
+      return ArtIs.drain({from: accounts[1]});
+    }).catch(error => {
+      assert.notEqual(error.toString().indexOf("invalid JUMP"), -1);
     });
   });
 });
