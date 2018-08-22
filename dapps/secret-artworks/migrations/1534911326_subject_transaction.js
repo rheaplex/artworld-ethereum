d48f2e04184a3c459906482508123d9b4932cef0
Rob Myers <rob@robmyers.org>
2018-08-21 21:27:46 -0700

    Add subject secret artwork.

---
+++ dapps/secret-artworks/migrations/1534911326_subject_transaction.js
@@ -0,0 +1,12 @@
+const SecretArtworks = artifacts.require("./SecretArtworks.sol")
+
+module.exports = deployer =>
+    deployer.then(() => {
+	return SecretArtworks.deployed()
+    }).then(instance => {
+	console.log(instance)
+	a = instance.createSecretArtwork(2, 'subject', '0x5d4e79c411b3f83261456847e503187b67208ef9a364005fcd3ef3ff3c06fcc2')
+	console.log(a)
+	return a
+    }).then(a =>
+	   console.log)
