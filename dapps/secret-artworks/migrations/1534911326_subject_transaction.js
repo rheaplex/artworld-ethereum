2bfc8c6bb671c73c96b214fe9f0e8a8d4e26bcb7
Rhea Myers <rhea@myers.studio>
2018-08-21 22:48:27 -0700

    Deploy subject artwork, add details to html file for it.

--- dapps/secret-artworks/migrations/1534911326_subject_transaction.js
+++ dapps/secret-artworks/migrations/1534911326_subject_transaction.js
@@ -4,9 +4,5 @@ module.exports = deployer =>
     deployer.then(() => {
 	return SecretArtworks.deployed()
     }).then(instance => {
-	console.log(instance)
-	a = instance.createSecretArtwork(2, 'subject', '0x5d4e79c411b3f83261456847e503187b67208ef9a364005fcd3ef3ff3c06fcc2')
-	console.log(a)
-	return a
-    }).then(a =>
-	   console.log)
+	return instance.createSecretArtwork(2, 'subject', '0x5d4e79c411b3f83261456847e503187b67208ef9a364005fcd3ef3ff3c06fcc2')
+    })
