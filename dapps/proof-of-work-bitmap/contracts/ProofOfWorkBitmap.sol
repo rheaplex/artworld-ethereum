0f5d74374409ad4ca768ebf4fb52e90f3cee2e50
Rhea Myers <rhea@myers.studio>
2019-07-04 17:42:26 -0700

    Ready to calculate test hash for PoW Bitmap.

--- dapps/proof-of-work-bitmap/contracts/ProofOfWorkBitmap.sol
+++ dapps/proof-of-work-bitmap/contracts/ProofOfWorkBitmap.sol
@@ -1,41 +1,48 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.5.8;
 
 //FIXME: make transferrible & drainable
 
 contract ProofOfWorkBitmap {
 
-    uint256 public bitmap;
+    bytes32 public bitmap;
     uint256 public nonce;
-    uint256 public hash;
+    bytes32 public hash;
     uint256 public height;
 
     event BitmapChanged(
         address by,
-        uint256 bitmap,
-        uint256 hash,
-        uint256 nonce
+        bytes32 bitmap,
+        uint256 nonce,
+        bytes32 hash
     );
 
-    function min(uint a, uint b) private pure returns (uint) {
-        return a < b ? a : b;
+    function max(uint a, uint b) private pure returns (uint) {
+        return a > b ? a : b;
     }
 
     // The number of *bytes* at the end of the hash that must be zero
-    // This must always be at least 1, and has a maximum of 8 when the bitmap
+    // This must always be at least 1, and has a maximum of 4 when the bitmap
     // is first set.
     // It reduces slowly over the next many blocks.
     function difficulty() public view returns (uint8 level) {
-        level = uint8(min(1, (8 - ((block.number - height) / 10))));
+        level = uint8(max(1, 4 - ((block.number - height) / 12)));
     }
 
     function validatePow(
-        uint256 newBitmap,
+        bytes32 newBitmap,
         uint256 newNonce,
-        uint256 newHash
-    ) public view returns ( bool valid ) {
+        bytes32 newHash
+    )
+        public view
+        returns (bool valid)
+    {
         // Is the hash correct?
-        bytes32 bitmapNonceHash = sha256(hash, newBitmap, newNonce);
-        valid = (newHash == uint256(bitmapNonceHash));
+        bytes32 bitmapNonceHash = keccak256(abi.encode(
+           hash,
+           newBitmap,
+           newNonce
+        ));
+        valid = (bitmapNonceHash == newHash);
         // Does the hash have the correct number of trailing zero bytes?
         if (valid) {
             for (uint256 i = 31; i > 31 - difficulty(); i--) {
@@ -50,17 +57,19 @@ contract ProofOfWorkBitmap {
     // Check that the provided proof of work / puzzle solution is valid.
     // If so, set the bitmap.
     function setBitmap (
-        uint256 newBitmap,
+        bytes32 newBitmap,
         uint256 newNonce,
-        uint256 newHash
-    ) public returns ( bool ) {
-        bool ok = validatePow(newBitmap, newNonce, newHash);
-        if (ok) {
-            bitmap = newBitmap;
-            nonce = newHash;
-            hash = newHash;
-            height = block.number;
-        }
-        return ok;
+        bytes32 newHash
+    )
+        public
+    {
+        require(
+            validatePow(newBitmap, newNonce, newHash),
+            "invalid proof-of-work"
+        );
+        bitmap = newBitmap;
+        nonce = newNonce;
+        hash = newHash;
+        height = block.number;
     }
 }
