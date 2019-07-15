fb627d1d7e225d0344d4f4811cdffb71398ea2bf
Rhea Myers <rhea@myers.studio>
2019-07-14 23:20:35 -0700

    Update gui and contract handling in proof-of-work-bitmap: it still needs bitmap updating to be properly implemented. Add semicolons, as adding them when needed is worse than adding parentheses when needed.

--- dapps/proof-of-work-bitmap/app/js/bitmap.js
+++ dapps/proof-of-work-bitmap/app/js/bitmap.js
@@ -16,152 +16,207 @@
 */
 
 
-const symbol = '☣'
+const symbol = '☣';
+
+const bitmapFromHexString = bitmapHex => {
+  const bitmapBits = bitmapHex
+          .match(/.{2}/g)
+          .map(x => parseInt(x, 16).toString(2).split(''));
+  return [].concat(...bitmapBits);
+};
+
+const hexStringFromBitmap = bitmapArray => bitmapArray.join('')
+      .match(/.{8}/g)
+      .map(x => parseInt(x, 2).toString(16))
+      .join('');
 
 class PixelsDisplay {
   indexForCoords (x, y) {
-    return (y * 16) + x
+    return (y * 16) + x;
   }
-  
+
   drawPixels (pixels, ctx, shapeFun, cellSize, on, off) {
     for (let y = 0; y < 16; y++) {
       for (let x = 0; x < 16; x++) {
-        const value = pixels[this.indexForCoords(x, y)]
+        const value = pixels[this.indexForCoords(x, y)];
         if (value == '1') {
-          ctx.fillStyle = on
+          ctx.fillStyle = on;
         } else {
-          ctx.fillStyle = off
+          ctx.fillStyle = off;
         }
-        const h = x * cellSize
-        const v = y * cellSize
-        shapeFun(ctx, h, v, cellSize)
+        const h = x * cellSize;
+        const v = y * cellSize;
+        shapeFun(ctx, h, v, cellSize);
       }
     }
   }
 
   drawRect (ctx, h, v, cellSize) {
-    ctx.beginPath()
-    ctx.rect(h, v, h + cellSize, v + cellSize)
-    ctx.fill()
+    ctx.beginPath();
+    ctx.rect(h, v, h + cellSize, v + cellSize);
+    ctx.fill();
   }
 
   drawCircle (ctx, h, v, cellSize) {
-    const radius = cellSize / 2
-    ctx.beginPath()
-    ctx.arc(h + radius, v + radius, radius, 0, 2 * Math.PI, false)
-    ctx.fill()
+    const radius = cellSize / 2;
+    ctx.beginPath();
+    ctx.arc(h + radius, v + radius, radius, 0, 2 * Math.PI, false);
+    ctx.fill();
   }
 
   drawSymbol (ctx, h, v, cellSize) {
-    ctx.font = "" + cellSize + "px sans"
-    ctx.fillText(symbol, h, v + cellSize)
+    ctx.font = "" + cellSize + "px sans";
+    ctx.fillText(symbol, h, v + cellSize);
   }
 
   drawPixelsRepresentation (pixelValues) {
-    const canvas = document.getElementById("#bitmap")
-    const ctx = canvas.getContext("2d")
-    canvas.width  = 1600
-    canvas.height = 1600
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    drawPixels(pixelValues, ctx, drawSymbol, canvas.width / 16, 'black',
-               'white')
+    const canvas = document.getElementById("bitmap");
+    const ctx = canvas.getContext("2d");
+    canvas.width  = 1600;
+    canvas.height = 1600;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    this.drawPixels(
+      bitmapFromHexString(pixelValues),
+      ctx,
+      this.drawSymbol,
+      canvas.width / 16,
+      'black',
+      'white'
+    );
   }
 }
 
-class PixelsGui {
-  constructor (pixelsContract) {
-    this.pixelsContract = pixelsContract
-    this.guiPixelValues = undefined
+class PixelsGui extends Gui {
+  constructor (pow) {
+    super();
+    this.pow = pow;
+    this.guiPixelValues = new Array(256).fill(0);
+    this.createGuiBitmap();
+    this.onClickShowGui('bitmap');
+    this.onClickHideGui('bitmap-gui-cancel');
+    document.getElementById('bitmap-gui-update')
+      .onclick = () => this.submitClickHandler();
   }
-    
+
   guiClickHandler (i) {
-    return function () {
-      if (this.guiPixelValues[i] == "1") {
-        this.guiPixelValues[i] = "0"
-      } else {
-        this.guiPixelValues[i] = "1"
-      }
-      this.setGuiBitmapState()
+    if (this.guiPixelValues[i] == "1") {
+      this.guiPixelValues[i] = "0";
+    } else {
+      this.guiPixelValues[i] = "1";
     }
+    this.setGuiBitmapState();
   }
   
   createGuiBitmap () {
-    const bitmap = document.getElementById("#bitmap-gui-bitmap")
+    const bitmap = document.getElementById("bitmap-gui-bitmap");
     for (let i = 0; i < 256; i++) {
-      const id = "gui-pixel-" + i
-      pixel = document.createElement('<div id="'
-                                     + id
-                                     + '" class="bitmap-gui-pixel"></div>')
-      bitmap.appendChild(pixel)
-      pixel.on('click', this.guiClickHandler(i))
+      const pixel = document.createElement('div');
+      pixel.className = 'bitmap-gui-pixel';
+      pixel.id = `gui-pixel-${i}`;
+      bitmap.appendChild(pixel);
+      pixel.onclick = () => this.guiClickHandler(i);
     }
   }
 
   setGuiBitmapState () {
-    const bitmap = document.getElementById("#bitmap-gui-bitmap")
+    const bitmap = document.getElementById("bitmap-gui-bitmap");
     for (let i = 0; i < 256; i++) {
-      const pixel = document.getElementById("#gui-pixel-" + i)
+      const pixel = document.getElementById("gui-pixel-" + i);
       if (this.guiPixelValues[i] == "1") {
-        pixel.addClass("bitmap-gui-pixel-on")
+        pixel.classList.add("bitmap-gui-pixel-on");
       } else {
-        pixel.removeClass("bitmap-gui-pixel-on")
+        pixel.classList.remove("bitmap-gui-pixel-on");
       }
     }
   }
   
-  async guiDisplayHookFun () {
-    const bitmapHex = await pixelsContract.getBitmap()
-    this.guiPixelValues = this.bitmapFromHexString(bitmapHex)
-    this.setGuiBitmapState()
+  async showGui () {
+    const bitmapHex = await this.pow.getBitmap();
+    this.guiPixelValues = bitmapFromHexString(bitmapHex);
+    this.setGuiBitmapState();
+    super.showGui();
   }
 
-  bitmapFromHexString (bitmapHex) {
-    const bitmapBits = bitmapHex
-          .match(/.{2}/g)
-          .map(x => parseInt(x, 16).toString(2).split(''))
-    return [].concat(...bitmapBits)
+  async submitClickHandler () {
+    this.hideGui();
+    this.pow.setBitmap(this.guiPixelValues);
   }
-
-  hexStringFromBitmap (bitmapArray) {
-    bitmapArray.join('')
-      .match(/.{8}/g)
-      .map(x => parseInt(x, 2).toString(16))
-      .join('')
-  }
-
 }
 
-class ProofOfWorkPixels is EthereumContractAccessor {
-  constructor () {
-    this.initializeWeb3()
-    if (this.initialized) {
-      this.pixelsContract = this.loadContract('../build/contracts/ProofOfWorkPixels.js')
-    } else {
-      // TELL USER
-    }
-  }
-  
-  async getBitmap () {
-    return (await this.pixelsContract.bitmap()).toString()
+class ProofOfWorkPixels extends EthereumNetwork {
+  async loadContracts() {
+    this.cancel = false;
+    this.pixelsContract = await this.loadContract(
+      '../build/contracts/ProofOfWorkBitmap.json'
+    );
   }
 
-  async setBitmap (bitmap, nonce, hash) {
-    return await this.pixelsContract.setBitmap(bitmap, nonce, hash)
+  cancelCalculatePow() {
+    this.cancel = true;
+  }
+
+  calculatePow (previousHash, bitmap, difficulty) {
+    const one = web3.utils.toBN('1');
+    const zeros = '0'.repeat(difficulty * 2);
+    let nonce = web3.utils.toBN(1);
+    let digest;
+    for(;! this.cancel; nonce = nonce.add(one)) {
+      console.log('.');
+      digest = web3.utils.soliditySha3(
+        {t: 'bytes32', v: previousHash},
+        {t: 'bytes32', v: bitmap},
+        {t: 'uint256', v: nonce}
+      )
+      if (digest.endsWith(zeros)) {
+        break
+      }
+    }
+    this.cancel = false;
+    return [digest, `0x${nonce.toString('hex')}`]
+  }
+
+  async getBitmap () {
+    return await this.pixelsContract.methods.bitmap().call();
+  }
+
+  async setBitmap (bitmap) {
+    const previousHash = await this.pixelsContract.methods.hash().call();
+    let difficulty = await this.pixelsContract.methods.difficulty()
+        .call();
+    let newHash;
+    let newNonce;
+    while (true) {
+      [newHash, newNonce] = this.calculatePow(previousHash, bitmap, difficulty);
+      // Try to catch the difficulty changing while we're calculating.
+      // The difficulty can still change between now and us broadcasting the
+      // results of this calculation to the network, but this reduces that
+      // window.
+      const currentDifficulty = await this.pixelsContract.difficulty().call();
+      if(currentDifficulty === difficulty) {
+        break;
+      } else {
+        difficulty = currentDifficulty;
+      }
+    }
+    return await this.pixelsContract.methods.setBitmap(
+      hexStringFromBitmap(this.guiPixmapValues),
+      newNonce,
+      newHash
+    ).send();
   }
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const connection = new Web3Connection()
-  connection.initializeWeb3()
-  if (! connection.initialized) {
-    document.write('Could not connect to Ethereum network')
+  const pow = await new ProofOfWorkPixels();
+  pow.initializeWeb3();
+  if (! pow.initialized) {
+    document.write('Could not connect to Ethereum network');
   } else {
-    const pixels = new ProofOfWorkPixels()
-    const display = new PixelsDisplay(pixels)
-    const gui = new PixelsGui(pixels)
-    gui.createGuiBitmap()
+    await pow.loadContracts();
+    const display = new PixelsDisplay(pow);
+    const gui = new PixelsGui(pow);
     //TODO: LISTEN TO PIXEL EVENTS
-    const pixels = await pixels.getPixels()
-    display.drawPixelsRepresentation()
+    const bitmap = await pow.getBitmap()
+    display.drawPixelsRepresentation(bitmap);
   }
-}
+});
