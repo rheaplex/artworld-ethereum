0957b967b92325d200958cdf24c05387018be16f
Rob Myers <rob@robmyers.org>
2019-08-10 17:22:17 -0700

    Pay Previous Path - feature complete but needing tidying up. I used a simple SVG path d text format for paths as an editor proved too involved. Some UX and centreing the path still to do.

---
+++ dapps/pay-previous-path/app/js/ppp.js
@@ -0,0 +1,282 @@
+/*  Hack Line Properties - Line stroke properties.
+    Copyright (C) 2019 Rob Myers <rob@robmyers.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+const pathSpecToD = (spec) => {
+  let d = [];
+  for(let i = 0; i < 32; i++) {
+    const commandSpec = parseInt(spec.command[i]);
+    if (commandSpec === 0) {
+      break;
+    }
+    // Lower 4 bits identify drawing commands
+    const command = commandSpec & 0x0F;
+    // Bit 6 identifies relative commands
+    const relative = spec.command[i] & 64;
+    // Bit 7 allows us to add a closespec after the command
+    const closepath = spec.command[i] & 128; 
+    switch(command) {
+    case 1:
+      d.push(`${relative? 'm' : 'M'} ${spec.x[i]} ${spec.y[i]}`);
+      break;
+    case 2:
+      d.push(`${relative? 'l' : 'L'} ${spec.x[i]} ${spec.y[i]}`);
+      break;
+    case 3:
+      d.push(`${relative? 'c' : 'C'} ${spec.x1[i]} ${spec.y1[i]} ${spec.x2[i]} ${spec.y2[i]} ${spec.x[i]} ${spec.y[i]}`);
+      break;
+    }
+    if (closepath) {
+      d.push(' Z');
+    }
+  }
+  return d.join(' ');
+};
+
+// Handrolled parser with regexes antipattern
+
+const ensureInt = (i) => {
+  if(isNaN(i)) {
+    throw new Error(`Not a number: ${i}`);
+  }
+  let val = parseInt(i);
+  // Clamp to range of signed 8-bit integer
+  if (val > 127) {
+    val = 127;
+  } else if (val < -128) {
+    val = -128;
+  }
+  return val;
+};
+
+const dToPathSpec = (d) => {
+  const spec = {
+    command: new Array(32).fill(0),
+    x1: new Array(32).fill(0),
+    y1: new Array(32).fill(0),
+    x2: new Array(32).fill(0),
+    y2: new Array(32).fill(0),
+    x: new Array(32).fill(0),
+    y: new Array(32).fill(0),
+  };
+  const tokens = d.match(/([a-zA-Z]|-?[0-9.]+)/g).reverse();
+  let index = 0;
+  while(tokens.length > 0 && index < 32) {
+    const command = tokens.pop();
+    if(! isNaN(command)) {
+      throw new Error(`Not a command: ${command}`);
+    }
+    switch(command) {
+    case 'Z':
+    case 'z':
+      if (index > 0) {
+        spec.command[index] |= 128;
+      }
+      break;
+    case 'M':
+      if(tokens.length < 2) {
+        throw new Error(`Too few numbers for M`);
+      }
+      spec.command[index] = 1;
+      spec.x[index] = ensureInt(tokens.pop());
+      spec.y[index] = ensureInt(tokens.pop());
+      index++;
+      break;
+    case 'm':
+      if(tokens.length < 2) {
+        throw new Error(`Too few numbers for m`);
+      }
+      spec.command[index] = 129;
+      spec.x[index] = ensureInt(tokens.pop());
+      spec.y[index] = ensureInt(tokens.pop());
+      index++;
+      break;
+    case 'L':
+      if(tokens.length < 2) {
+        throw new Error(`Too few numbers for L`);
+      }
+      spec.command[index] = 2;
+      spec.x[index] = ensureInt(tokens.pop());
+      spec.y[index] = ensureInt(tokens.pop());
+      index++;
+      break;
+    case 'L':
+      if(tokens.length < 2) {
+        throw new Error(`Too few numbers for L`);
+      }
+      spec.command[index] = 130;
+      spec.x[index] = ensureInt(tokens.pop());
+      spec.y[index] = ensureInt(tokens.pop());
+      index++;
+      break;
+    case 'C':
+      if(tokens.length < 6) {
+        throw new Error(`Too few numbers for C`);
+      }
+      spec.command[index] = 3;
+      spec.x1[index] = ensureInt(tokens.pop());
+      spec.y1[index] = ensureInt(tokens.pop());
+      spec.x2[index] = ensureInt(tokens.pop());
+      spec.y2[index] = ensureInt(tokens.pop());
+      spec.x[index] = ensureInt(tokens.pop());
+      spec.y[index] = ensureInt(tokens.pop());
+      index++;
+      break;
+    case 'c':
+      if(tokens.length < 6) {
+        throw new Error(`Too few numbers for c`);
+      }
+      spec.command[index] = 131;
+      spec.x1[index] = ensureInt(tokens.pop());
+      spec.y1[index] = ensureInt(tokens.pop());
+      spec.x2[index] = ensureInt(tokens.pop());
+      spec.y2[index] = ensureInt(tokens.pop());
+      spec.x[index] = ensureInt(tokens.pop());
+      spec.y[index] = ensureInt(tokens.pop());
+      index++;
+      break;
+    }
+  }
+  return spec;
+};
+
+
+class PathDisplay {
+  constructor (path) {
+    this.path = path;
+    this.path.registerPathChangedHandler(
+      this.drawPathRepresentationEvent.bind(this)
+    );
+  }
+
+  drawPathRepresentationEvent (event) {
+    const d = pathSpecToD(event.returnValues);
+    this.drawPathRepresentation(d);
+  }
+
+  drawPathRepresentation (d) {
+    document.getElementById('path').setAttribute('d', d);
+  }
+}
+
+class PathGui extends Gui {
+  constructor (path) {
+    super();
+    this.path = path;
+    this.spec = {};
+    this.onClickShowGui('representation');
+    this.onClickHideGui('path-gui-cancel');
+    document.getElementById('path-gui-update')
+      .onclick = this.submitClickHandler.bind(this);
+    document.getElementById('path-gui-d')
+      .addEventListener('input', this.setFromD.bind(this));
+  }
+
+  // super.showGui() isn't async, but that's OK
+
+  async showGui () {
+    const spec = await this.path.getBlockchainPath();
+    this.spec = spec;
+    document.getElementById('path-gui-d').value = pathSpecToD(this.spec);
+    document.getElementById('path-gui-d-error').innerText = '';
+    this.drawPathRepresentation();
+    super.showGui();
+  }
+
+  setFromD () {
+    const dField = document.getElementById('path-gui-d');
+    const dIn = dField.value;
+    try {
+      this.spec = dToPathSpec(dIn);
+      dField.value = pathSpecToD(this.spec);
+      this.drawPathRepresentation();
+      document.getElementById('path-gui-d-error').innerText = '';
+    } catch (e) {
+      document.getElementById('path-gui-d-error').innerText = e.message;
+    }
+  }
+  
+  async submitClickHandler () {
+    this.hideGui();
+    this.showUpdating();
+    try {
+      await this.path.setBlockchainPath(this.spec);
+    } catch (e) {
+      //FIXME: ERROR, BUT NOT IF USER CANCELLED
+    } finally {
+      this.hideUpdating();
+    }
+  }
+
+  drawPathRepresentation () {
+    const d = pathSpecToD(this.spec);
+    document.getElementById('path-gui-path').setAttribute('d', d);
+  }
+}
+
+class PayPreviousPath extends EthereumNetwork {
+  async loadContracts() {
+    this.pathContract = await this.loadContract(
+      '../build/contracts/PayPreviousPath.json'
+    );
+  }
+
+  async getBlockchainPath () {
+    return this.pathContract.methods.getPath().call();
+  }
+
+  async setBlockchainPath (spec) {
+    const account = await this.tryForAccountAccess();
+    const gasPrice = await web3.eth.getGasPrice();
+    const fee = await this.pathContract.methods.calculateFee(gasPrice).call();
+    return this.pathContract.methods.setPath(
+      spec.command,
+      spec.x1,
+      spec.y1,
+      spec.x2,
+      spec.y2,
+      spec.x,
+      spec.y
+    ).send({
+      from: account,
+      value: fee,
+      gasPrice: gasPrice
+    });
+  }
+
+  registerPathChangedHandler (callback) {
+    this.pathContract.events.PathChanged().on('data', callback);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Lifecycle
+////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const path = new PayPreviousPath();
+  path.initializeWeb3();
+  if (! path.initialized) {
+    document.write('Could not connect to Ethereum network');
+  } else {
+    await path.loadContracts();
+    const display = new PathDisplay(path);
+    const gui = new PathGui(path);
+    const d = pathSpecToD(await path.getBlockchainPath());
+    display.drawPathRepresentation(d);
+  }
+});
--- dapps/pay-previous-path/contracts/PayPreviousPath.sol
+++ dapps/pay-previous-path/contracts/PayPreviousPath.sol
@@ -1,54 +1,149 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.5.0;
 
-contract PayPreviousPath {
 
-    int8[32] public pointX;
-    int8[32] public pointY;
-    // This would be an enum if compiler & ABI support made it easier
-    uint8[32] public pointType;
+import "@openzeppelin/contracts/payment/PullPayment.sol";
+
+
+/*
+  NOTES
+  =====
+  - The x1 y1 x2 y2 x y order is from the SVG standard.
+  - We use PullPayment because this works with real Ether.
+ */
+
+
+contract PayPreviousPath is PullPayment {
+    // Use the low 4 bits for command, the high 4 bits for flags.
+    // (We don't fill this space, but it's the schema).
+    uint8 constant public T_MOVETO = 1;
+    uint8 constant public T_LINETO = 2;
+    uint8 constant public T_CURVETO_BEZIER = 3;
+    // Not used but here in case
+    uint8 constant public T_CURVETO_QUADRATIC = 4;
+    // Added to another command, use the relative rather than absolute kind.
+    // We don't use this but it's here as future proofing.
+    uint8 constant public T_RELATIVE = 1 << 6;
+    // On its own, closepath.
+    // Added to another command, executes after the command it's added to.
+    uint8 constant public T_CLOSEPATH = 1 << 7;
+
+    event PathChanged(
+        address indexed by,
+        uint8[32] command,
+        int8[32] x1,
+        int8[32] y1,
+        int8[32] x2,
+        int8[32] y2,
+        int8[32] x,
+        int8[32] y
+    );
+
+    // In this house we use Bezier Curves.
+    // This representation is unstructured and inflexible but clear and easy to
+    // process. We don't want to allow invalid d="" specs or have to parse them
+    // to validate them, so a string or bytes wouldn't work.
+    // Just using Quadratics would take less room, the svg path T command would
+    // take even less, but these remove control/fidelity/possibility.
+    // The pointCommand is almost redundant, we could have a single path
+    // consisting of Cs with an implicit M 0 0, but we want to be able to
+    // represent more complex paths.
+    uint8[32] public pointCommand;
+    int8[32] public  pointX1;
+    int8[32] public  pointY1;
+    int8[32] public  pointX2;
+    int8[32] public  pointY2;
+    int8[32] public  pointX;
+    int8[32] public  pointY;
 
     address public previousPayer;
-    uint256 public previousPayerAmount;
 
-    mapping (address => uint256) public pendingWithdrawals;
-
-    event Path(int8[32] x, int8[32] y, uint8[32] t);
-
-    function PayPreviousPath() public {
+    constructor() public {
         previousPayer = msg.sender;
-        // Start with a payment to the contract's creator
-        previousPayerAmount = 0.001 ether;
-        pendingWithdrawals[previousPayer] = previousPayerAmount;
+        //FIXME: TEST PATH
+        pointCommand[0] = 1;
+        pointX[0] = 10;
+        pointY[0] = 20;
+        pointCommand[1] = 2;
+        pointX[1] = 80;
+        pointY[1] = 120;
+        pointCommand[2] = 3;
+        pointX1[2] = 12;
+        pointY1[2] = 30;
+        pointX2[2] = 40;
+        pointY2[2] = 60;
+        pointX[2] = 120;
+        pointY[2] = 20;
+        pointCommand[3] = 2 + T_CLOSEPATH;
+        pointX[3] = 33;
+        pointY[3] = 14;
+
     }
 
-    function setPath(int8[32] x, int8[32] y, uint8[32] t) public payable {
-        //require(msg.value > previousPayerAmount);
-        //NOTE: we explicitly do not disallow the same sender setting the path
-        //      multiple times (they may wish to change it), but we do not have
-        //      any special logic to handle this. The sender must have the
-        //      total amount for both payments available.
-        pendingWithdrawals[previousPayer] += previousPayerAmount;
-        previousPayer = msg.sender;
-        previousPayerAmount = (msg.value - previousPayerAmount);
-        pointX = x;
-        pointY = y;
-        pointType = t;
-        Path(pointX, pointY, pointType);
+    function calculateFee (uint256 _gasPrice) public pure returns (uint256) {
+        // This locks in the base transaction price but it's a schellingpoint
+        return 2300 * _gasPrice;
     }
 
-    function getCurrentPath() public view
-        returns (int8[32] x, int8[32] y, uint8[32] t) {
+    function hasCorrectFee () internal returns (bool) {
+
+        return msg.value == calculateFee(tx.gasprice);
+    }
+
+    //NOTE: We don't catch the case of setting the path to itself.
+
+    function setPath(
+        uint8[32] memory _command,
+        int8[32] memory _x1,
+        int8[32] memory _y1,
+        int8[32] memory _x2,
+        int8[32] memory _y2,
+        int8[32] memory _x,
+        int8[32] memory _y
+    )
+        public
+        payable
+    {
+        // Pay the fee to the previous path setter
+        uint256 fee = calculateFee(tx.gasprice);
+        require(msg.value == fee, "Incorrect fee");
+        _asyncTransfer(previousPayer, fee);
+        previousPayer = msg.sender;
+        // Set the path
+        pointCommand = _command;
+        pointX1 = _x1;
+        pointY1 = _y1;
+        pointX2 = _x2;
+        pointY2 = _y2;
+        pointX = _x;
+        pointY = _y;
+        emit PathChanged(
+            msg.sender,
+            pointCommand,
+            pointX1,
+            pointY1,
+            pointX2,
+            pointY2,
+            pointX,
+            pointY
+        );
+    }
+
+    function getPath() external view
+    returns (
+        uint8[32] memory command,
+        int8[32] memory x1,
+        int8[32] memory y1,
+        int8[32] memory x2,
+        int8[32] memory y2,
+        int8[32] memory x,
+        int8[32] memory y
+    ) {
+        command = pointCommand;
+        x1 = pointX1;
+        y1 = pointY1;
+        x2 = pointX2;
+        y2 = pointY2;
         x = pointX;
         y = pointY;
-        t = pointType;
     }
-
-    function withdrawPreviousPayments() public {
-        uint256 amount = pendingWithdrawals[msg.sender];
-        if(amount > 0) {
-            pendingWithdrawals[msg.sender] -= amount;
-            msg.sender.transfer(amount);
-        }
-    }
-
 }
