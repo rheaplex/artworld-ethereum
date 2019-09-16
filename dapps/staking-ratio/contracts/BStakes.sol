6bebfaecbc885d39302f7b594ebeecd4efb4867d
Rhea Myers <rhea@myers.studio>
2019-09-15 17:27:44 -0700

    Staking Ratio: convert to use OpenZeppelin Escrow classes and start on UI.

---
+++ dapps/staking-ratio/contracts/BStakes.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+
+import '@openzeppelin/contracts/payment/escrow/Escrow.sol';
+
+
+contract BStakes is Escrow {}
--- dapps/staking-ratio/test/staking_ratio.js
+++ dapps/staking-ratio/test/staking_ratio.js
@@ -1,75 +1,91 @@
-/* globals artifacts, assert, contract, it, web3 */
+/*global artifacts, assert, contract, it, web3*/
 
-const StakingRatio = artifacts.require('./StakingRatio.sol')
+const StakingRatio = artifacts.require('./StakingRatio.sol');
 
-const aAmount = web3.toWei(0.1, 'ether')
-const bAmount = web3.toWei(0.2, 'ether')
+const aAmount = web3.utils.toBN(web3.utils.toWei('0.1', 'ether'));
+const bAmount = web3.utils.toBN(web3.utils.toWei('0.2', 'ether'));
+
+//FIXME: There are some order dependencies in these tests.
 
 contract('StakingRatio', function(accounts) {
 
   it('should start with zero as A and B', async function() {
-    const instance = await StakingRatio.deployed()
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), 0)
-    const b = await instance.bAmount.call()
-    assert.equal(b.toNumber(), 0)
-  })
+    const instance = await StakingRatio.deployed();
+    const a = await instance.aAmount.call();
+    assert.equal(a.toNumber(), 0);
+    const b = await instance.bAmount.call();
+    assert.equal(b.toNumber(), 0);
+  });
 
   it('should allow the user to stake both A and B', async function() {
-    const instance = await StakingRatio.deployed()
-    await instance.stakeA.sendTransaction({from: accounts[1], value: aAmount})
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), aAmount)
-    await instance.stakeB.sendTransaction({from: accounts[1], value: bAmount})
-    const b = await instance.bAmount.call()
-    assert.equal(b.toNumber(), bAmount)
-  })
+    const instance = await StakingRatio.deployed();
+    await instance.stakeA.sendTransaction({from: accounts[1], value: aAmount});
+    const a = await instance.aAmount.call();
+    assert.ok(a.eq(aAmount));
+    const aStake = await instance.myAStake({ from: accounts[1] });
+    assert.ok(aStake.eq(aAmount));
+    await instance.stakeB.sendTransaction({from: accounts[1], value: bAmount});
+    const b = await instance.bAmount.call();
+    assert.ok(b.eq(bAmount));
+    const bStake = await instance.myBStake({ from: accounts[1] });
+    assert.ok(bStake.eq(bAmount));
+  });
 
   it('should allow the user to stake A and B multiple times', async function() {
-    const instance = await StakingRatio.deployed()
-    await instance.stakeA.sendTransaction({from: accounts[1], value: aAmount})
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), aAmount * 2)
-    await instance.stakeB.sendTransaction({from: accounts[1], value: bAmount})
-    const b = await instance.bAmount.call()
-    assert.equal(b.toNumber(), bAmount * 2)
-  })
-
+    const instance = await StakingRatio.deployed();
+    await instance.stakeA.sendTransaction({from: accounts[1], value: aAmount});
+    const a = await instance.aAmount.call();
+    assert.ok(a.eq(aAmount.mul(web3.utils.toBN(2))));
+    const aStake = await instance.myAStake({ from: accounts[1] });
+    assert.ok(aStake.eq(aAmount.mul(web3.utils.toBN(2))));
+    await instance.stakeB.sendTransaction({from: accounts[1], value: bAmount});
+    const b = await instance.bAmount.call();
+    assert.ok(b.eq(bAmount.mul(web3.utils.toBN(2))));
+    const bStake = await instance.myBStake({ from: accounts[1] });
+    assert.ok(bStake.eq(bAmount.mul(web3.utils.toBN(2))));
+  });
 
   it('should allow multiple users to stake A and B', async function() {
-    const instance = await StakingRatio.deployed()
-    await instance.stakeA.sendTransaction({from: accounts[2], value: aAmount})
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), aAmount * 3)
-    await instance.stakeB.sendTransaction({from: accounts[2], value: bAmount})
-    const b = await instance.bAmount.call()
-    assert.equal(b.toNumber(), bAmount * 3)
-  })
+    const instance = await StakingRatio.deployed();
+    await instance.stakeA.sendTransaction({from: accounts[2], value: aAmount});
+    const a = await instance.aAmount.call();
+    assert.ok(a.eq(aAmount.mul(web3.utils.toBN(3))));
+    const aStake = await instance.myAStake({ from: accounts[2] });
+    assert.ok(aStake.eq(aAmount));
+    await instance.stakeB.sendTransaction({from: accounts[2], value: bAmount});
+    const b = await instance.bAmount.call();
+    assert.ok(b.eq(bAmount.mul(web3.utils.toBN(3))));
+    const bStake = await instance.myBStake({ from: accounts[2] });
+    assert.ok(bStake.eq(bAmount));
+  });
 
   it('should allow users with stake(s) to widthdraw', async function() {
-    const instance = await StakingRatio.deployed()
-    await instance.withdrawA.sendTransaction(aAmount, {from: accounts[1]})
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), aAmount * 2)
-    await instance.withdrawB.sendTransaction(bAmount, {from: accounts[1]})
-    const b = await instance.bAmount.call()
-    assert.equal(b.toNumber(), bAmount * 2)
-  })
+    const instance = await StakingRatio.deployed();
+    await instance.withdrawA.sendTransaction({from: accounts[1]});
+    const a = await instance.aAmount.call();
+    assert.ok(a.eq(aAmount));    
+    const aStake = await instance.myAStake({ from: accounts[1] });
+    assert.ok(aStake.eq(web3.utils.toBN(0)));
+    await instance.withdrawB.sendTransaction({from: accounts[1]});
+    const b = await instance.bAmount.call();
+    assert.ok(b.eq(bAmount));
+    const bStake = await instance.myBStake({ from: accounts[1] });
+    assert.ok(bStake.eq(web3.utils.toBN(0)));
+  });
 
   it('should not allow users with no stake to widthdraw', async function() {
-    const instance = await StakingRatio.deployed()
-    await instance.withdrawA.sendTransaction(aAmount, {from: accounts[3]})
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), aAmount * 2)
-  })
+    const instance = await StakingRatio.deployed();
+    await instance.withdrawA.sendTransaction({from: accounts[3]});
+    const a = await instance.aAmount.call();
+    assert.ok(a.eq(aAmount));
+  });
 
   it('should allow users to widthdraw all their stake(s)', async function() {
-    const instance = await StakingRatio.deployed()
-    await instance.withdrawAll.sendTransaction({from: accounts[2]})
-    const a = await instance.aAmount.call()
-    assert.equal(a.toNumber(), aAmount)
-    const b = await instance.bAmount.call()
-    assert.equal(b.toNumber(), bAmount)
-  })
-
-})
+    const instance = await StakingRatio.deployed();
+    await instance.withdrawAll.sendTransaction({from: accounts[2]});
+    const a = await instance.aAmount.call();
+    assert.ok(a.eq(web3.utils.toBN(0)));
+    const b = await instance.bAmount.call();
+    assert.ok(b.eq(web3.utils.toBN(0)));
+  });
+});
